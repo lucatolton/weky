beec48648cd99a43d7146a609846cb4e0e50e08c
ImFace <78975742+ImFace@users.noreply.github.com>
2021-08-01 13:52:36 +0300

    Update rip.js

--- commands/image/rip.js
+++ commands/image/rip.js
@@ -2,6 +2,7 @@
 
 const Discord = require('discord.js');
 const config = require('../../util/config.json');
+const canvacord = require('canvacord');
 
 module.exports.run = async (client, message, args, utils, data) => {
   const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]) || message.member;
