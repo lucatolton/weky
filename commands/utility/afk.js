1b69475717f413d6e0592d3c92c9070cc5036b41
ImFace <78975742+ImFace@users.noreply.github.com>
2021-08-03 13:43:31 +0300

    Update afk.js

--- commands/utility/afk.js
+++ commands/utility/afk.js
@@ -2,8 +2,8 @@
 const Discord = require('discord.js');
 module.exports.run = async (client, message, args, utils) => {
 	const afkreason = args.join(' ') || 'AFK';
-	
-	await client.data.setAfk(message.author.id, afkreason);
+
+	require('../../schemas/userDB').findOneAndUpdate({ id: message.author.id }, { is_afk: true, afkReason: afkreason }, { upset: true })
 	message.channel.send(Discord.Util.removeMentions(`You are now afk for: **\`${afkreason}\`**`));
 };
 
