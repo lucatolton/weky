e7763873ac38bfc49ec5f8202358a5dcc47d3897
ImFace <78975742+ImFace@users.noreply.github.com>
2021-08-03 21:14:00 +0300

    Update stats.js

--- commands/rpg/stats.js
+++ commands/rpg/stats.js
@@ -9,26 +9,26 @@ const ssSchema = require('../../schemas/spaceship');
 
 module.exports.run = async (client, message, args, utils, data) => {
 
-	rpgSchema.findOne({ id: message.author.id }, async (err, data) => {
+	rpgSchema.findOne({ id: message.author.id }, async (err, dat) => {
 		await ssSchema.findOne({ id: message.author.id }).lean().exec().then(async (dataShips) => {
-			if (!data || typeof data == null) {
-				return data.rpg.addUser(message.author.id, message)
+			if (!dat || typeof dat == null) {
+				return dat.rpg.addUser(message.author.id, message)
 			} else {
-				let equipedHero = data.hero.find((e) => e.heroEquiped === true)
+				let equipedHero = dat.hero.find((e) => e.heroEquiped === true)
 				let embed = new Discord.MessageEmbed()
 					.setAuthor(message.author.tag, message.author.displayAvatarURL())
 					.addField('In-Battle Stats',
-						`\`-\` Died **x${data.stats.diedCounter}** times.` +
-						`\`-\` Killed **x${data.stats.mobsKilled}** mobs.` +
-						`\`-\` Battled **x${data.stats.diedCounter + data.stats.mobsKilled}** times.` +
-						`\`-\` Total MN **x${data.stats.totalMn.toLocaleString('en')}**.`, true)
+						`\`-\` Died **x${dat.stats.diedCounter}** times.` +
+						`\`-\` Killed **x${dat.stats.mobsKilled}** mobs.` +
+						`\`-\` Battled **x${dat.stats.diedCounter + dat.stats.mobsKilled}** times.` +
+						`\`-\` Total MN **x${dat.stats.totalMn.toLocaleString('en')}**.`, true)
 					.addField('General',
 						`\`-\` Registred at <t:${Math.round(data.user.registeredAt / 1000)}:R>.` +
 						`\`-\` In <t:${dat.stats.isInSpaceShip ? '`' + dat.stats.inWhatSpaceShip + '`' : 'no spaceship'}.` +
 						`\`-\` Hero **${rpgdata.hero.find((u) => u.name.includes(equipedHero.name)).emoji + ' ' + equipedHero.name}**.` +
-						`\`-\` Planet **x${map.find((u) => u.planet.includes(data.stats.planet)).emoji + ' ' + data.stats.planet}**.` +
-						`\`-\` Total Planets **x${data.stats.planetsUnlocked.length}**.` +
-						`\`-\` Total Heroes **x${data.stats.hero.sort((a) => a.heroUnlocked == true).length}**.`, true)
+						`\`-\` Planet **x${map.find((u) => u.planet.includes(dat.stats.planet)).emoji + ' ' + dat.stats.planet}**.` +
+						`\`-\` Total Planets **x${dat.stats.planetsUnlocked.length}**.` +
+						`\`-\` Total Heroes **x${dat.stats.hero.sort((a) => a.heroUnlocked == true).length}**.`, true)
 				message.reply(embed)
 			}
 		})
