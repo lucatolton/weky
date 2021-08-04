1a2b33dae99b9ff384b868e49e7e4c429ed3c2ec
ImFace <78975742+ImFace@users.noreply.github.com>
2021-08-04 13:29:39 +0300

    :clap: a :clap: s :clap: y :clap: n :clap: c :clap:

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -71,7 +71,7 @@ module.exports.run = async (client, message, args, utils, data) => {
       .setDefault())
   })
 
-  message.channel.send(new Discord.MessageAttachment('https://cdn.discordapp.com/attachments/796339751044382720/872215137954697288/Untitled_11.jpg'), { components: theArrayThing }).then((msg) => {
+  message.channel.send(new Discord.MessageAttachment('https://cdn.discordapp.com/attachments/796339751044382720/872215137954697288/Untitled_11.jpg'), { components: theArrayThing }).then(async (msg) => {
 
     const collector = await msg.createMenuCollector(c => c)
 
