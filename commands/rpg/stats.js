0c4f98512290cdca51ef38c5499c868485c55e20
ImFace <78975742+ImFace@users.noreply.github.com>
2021-08-04 15:35:46 +0300

    Update stats.js

--- commands/rpg/stats.js
+++ commands/rpg/stats.js
@@ -6,7 +6,7 @@ const rpgSchema = require('../../schemas/rpg')
 
 module.exports.run = async (client, message, args, utils, data) => {
 	await rpgSchema.findOne({ id: message.author.id }).lean().exec().then(async (extractedData) => {
-        if (!data || typeof data == null) return client.data.rpg(message.author.id, message)
+        if (!extractedData || typeof extractedData == null) return client.data.rpg(message.author.id, message)
 	let embed = new Discord.MessageEmbed()
 		.setAuthor(message.author.tag, message.author.displayAvatarURL())
 		.addField('_ _', '**☠️ Deaths Counter**: \n' + extractedData.stats.diedCounter, true)
