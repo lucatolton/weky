9fd5046329bc5985c704c87eb6c7d09d576ae557
ImFace <78975742+ImFace@users.noreply.github.com>
2021-08-04 11:45:20 +0300

    fixed the spaecship category (data was undefined)

--- commands/spaceship/spaceship.js
+++ commands/spaceship/spaceship.js
@@ -1,21 +1,21 @@
 /* eslint-disable no-unused-vars */
 const Discord = require('discord.js');
 const ssSchema = require('../../schemas/spaceship');
+const rpgSchema = require('../../schemas/rpg')
 
 module.exports.run = async (client, message, args, utils, data) => {
+    await rpgSchema.findOne({ id: message.author.id }).lean().exec().then((extractedData) => {
 
-    let userx = await data.rpg.user(message.author.id, message)
+        if (extractedData.stats.isInSpaceShip == false) return message.reply('You don\'t! are not in any spaceships!')
+        const d = await ssSchema.findOne({ SpaceShipID: extractedData.stats.inWhatSpaceShip })
 
-    if (userx.db.stats.isInSpaceShip == false) return message.reply('You don\'t! are not in any spaceships!')
-    const d = await ssSchema.findOne({ SpaceShipID: userx.db.stats.inWhatSpaceShip })
+        if (!d) return message.reply('You don\'t! are not in any spaceships!')
 
-    if (!d) return message.reply('You don\'t! are not in any spaceships!')
+        const canvas = await utils.displaySs(d, client)
 
-    const canvas = await utils.displaySs(d, client)
-
-    const iconsAttachment = new Discord.MessageAttachment(canvas.toBuffer(), 'icons.png')
-    message.reply(iconsAttachment)
-
+        const iconsAttachment = new Discord.MessageAttachment(canvas.toBuffer(), 'icons.png')
+        message.reply(iconsAttachment)
+    })
 };
 
 module.exports.help = {
