db3e4de4416f3a5cf0af1c93d252345fb6b52d88
ImFace <78975742+ImFace@users.noreply.github.com>
2021-08-06 21:31:15 +0300

    Update chat.js

--- commands/spaceship/chat.js
+++ commands/spaceship/chat.js
@@ -19,8 +19,8 @@ module.exports.run = async (client, message, args, utils, data) => {
 
         } else {
             const messages = []
-            d.SpaceShipMessages.forEach((msg) => messages.push(`**${client.users.cache.get(msg.slice(0, 17)).tag}**: ${msg.slice(17)}`))
-            
+            d.SpaceShipMessages.forEach((msg) => { messages.push(`**${client.users.cache.get(msg.slice(0, 17)).tag}**: ${msg.slice(17)}`) })
+
             if (extractedData.stats.isInSpaceShip == false) return message.reply('You are not in any spaceship!')
             message.channel.send(
                 new Discord.MessageEmbed()
