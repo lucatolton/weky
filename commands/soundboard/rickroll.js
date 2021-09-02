20c2f19d872394a2b160cb69a649ca7323e244cb
LightningBolt62 <53604347+LightningBolt62@users.noreply.github.com>
2021-09-02 18:28:31 +0600

    Update rickroll.js

--- commands/soundboard/rickroll.js
+++ commands/soundboard/rickroll.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message) => {
 	if (!channel) return message.channel.send('Please connect to a voice channel to use soundboard');
 	function play() {
 		const player = createAudioPlayer();
-		const resource = createAudioResource(path.join(__dirname + '/audio/rickrol.mp3'));
+		const resource = createAudioResource(path.join(__dirname + '/audio/rickroll.mp3'));
 		const connection = joinVoiceChannel({
 			channelId: message.member.voice.channel.id,
 			guildId: message.guild.id,
